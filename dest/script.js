bbadefe8ef6c277251aef33852cbdeab43bc8e8a
Bethlehem-Tesfaye <bethlehem.tesfaye15@gmail.com>
2024-09-05 23:04:47 +0400

    update files
    to do list made to function properly

--- dest/script.js
+++ dest/script.js
@@ -2,45 +2,6 @@
 const list = document.querySelector("ul");
 const input = document.querySelector("form input");
 const button = document.querySelector("form button");
-const tasks = [];
-button === null || button === void 0 ? void 0 : button.addEventListener('click', function (e) {
-    e.preventDefault();
-    if ((input === null || input === void 0 ? void 0 : input.value) == "" || (input === null || input === void 0 ? void 0 : input.value) == null) {
-        return;
-    }
-    const newtask = {
-        title: input.value,
-        completed: false,
-        createdAt: new Date()
-    };
-    tasks.push(newtask);
-    addTask();
-    input.value = "";
-    console.log(tasks);
-    saveTask();
-});
-const addTask = function () {
-    if (list === null || list === void 0 ? void 0 : list.innerHTML) {
-        list.innerHTML = "";
-    }
-    tasks.forEach((task) => {
-        const taskList = document.createElement("li");
-        const div = document.createElement("div");
-        div.setAttribute("class", "list-container");
-        const checkBox = document.createElement("input");
-        checkBox.setAttribute("type", "checkbox");
-        const label = document.createElement("label");
-        label.append(checkBox, task.title);
-        taskList.append(label);
-        list === null || list === void 0 ? void 0 : list.append(taskList);
-        checkBox.checked = task.completed;
-        checkBox.addEventListener("change", () => {
-            task.completed = checkBox.checked;
-            console.log(tasks);
-            saveTask();
-        });
-    });
-};
 const saveTask = function () {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 };
@@ -57,5 +18,148 @@ const getTask = function () {
 document.addEventListener("DOMContentLoaded", () => {
     getTask();
     addTask();
+    updatedCount();
 });
+const tasks = [];
+button === null || button === void 0 ? void 0 : button.addEventListener('click', function (e) {
+    e.preventDefault();
+    if ((input === null || input === void 0 ? void 0 : input.value) == "" || (input === null || input === void 0 ? void 0 : input.value) == null) {
+        return;
+    }
+    const newtask = {
+        title: input.value,
+        completed: false,
+        createdAt: new Date()
+    };
+    tasks.push(newtask);
+    addTask();
+    input.value = "";
+    console.log(tasks);
+    saveTask();
+    updatedCount();
+});
+const addTask = function () {
+    if (list === null || list === void 0 ? void 0 : list.innerHTML) {
+        list.innerHTML = "";
+    }
+    tasks.forEach((task, index) => {
+        const taskList = document.createElement("li");
+        const div = document.createElement("div");
+        div.setAttribute("class", "list-container");
+        const label = document.createElement("label");
+        const checkBox = document.createElement("input");
+        checkBox.setAttribute("type", "checkbox");
+        if (checkBox.checked) {
+            label.setAttribute("class", "completed");
+            saveTask();
+        }
+        else
+            label.setAttribute("class", "");
+        label.append(checkBox, task.title);
+        const div2 = document.createElement("div");
+        div2.setAttribute("class", "div2");
+        div2.append(label);
+        const iconDiv = document.createElement("div");
+        iconDiv.setAttribute("class", "icons");
+        const deleteImg = document.createElement("img");
+        deleteImg.src = "../delete.png";
+        deleteImg.addEventListener("click", () => remove(index));
+        const editImg = document.createElement("img");
+        editImg.src = "../edit.png";
+        editImg.addEventListener("click", () => edit(index));
+        iconDiv.append(editImg, deleteImg);
+        div.append(div2, iconDiv);
+        taskList.append(div);
+        list === null || list === void 0 ? void 0 : list.append(taskList);
+        checkBox.checked = task.completed;
+        checkBox.addEventListener("change", () => {
+            task.completed = checkBox.checked;
+            console.log(tasks);
+            if (checkBox.checked) {
+                label.setAttribute("class", "completed");
+            }
+            else {
+                label.setAttribute("class", "");
+            }
+            saveTask();
+            updatedCount();
+        });
+        if (checkBox.checked) {
+            label.setAttribute("class", "completed");
+        }
+        else
+            label.setAttribute("class", "");
+    });
+};
+const remove = function (index) {
+    tasks.splice(index, 1);
+    addTask();
+    saveTask();
+    updatedCount();
+};
+const edit = function (index) {
+    if ((input === null || input === void 0 ? void 0 : input.value) == null) {
+    }
+    else {
+        input.value = tasks[index].title;
+        tasks.splice(index, 1);
+        addTask();
+        saveTask();
+        updatedCount();
+    }
+};
+const updatedCount = () => {
+    let count = document.querySelector(".task-count p");
+    let progress = document.querySelector(".progress");
+    let message = document.querySelector(".title p");
+    let totalTask = tasks.length;
+    let completedTask = tasks.filter(task => task.completed).length;
+    count.innerHTML = completedTask + " / " + totalTask;
+    if (totalTask == 0 && completedTask == 0) {
+        progress.style.width = "0%";
+    }
+    else {
+        progress.style.width = completedTask / totalTask * 100 + "%";
+    }
+    if (totalTask == completedTask && totalTask != 0 && completedTask != 0) {
+        message.innerHTML = "You Completed Your Tasks!!";
+        confettiEffect();
+    }
+    else {
+        message.innerHTML = "Keep it up";
+    }
+};
+const confettiEffect = () => {
+    const count = 200;
+    const defaults = {
+        origin: { y: 0.7 },
+    };
+    function fire(particleRatio, opts) {
+        window.confetti(Object.assign({}, defaults, opts, {
+            particleCount: Math.floor(count * particleRatio),
+        }));
+    }
+    fire(0.25, {
+        spread: 26,
+        startVelocity: 55,
+    });
+    fire(0.2, {
+        spread: 60,
+    });
+    fire(0.35, {
+        spread: 100,
+        decay: 0.91,
+        scalar: 0.8,
+    });
+    fire(0.1, {
+        spread: 120,
+        startVelocity: 25,
+        decay: 0.92,
+        scalar: 1.2,
+    });
+    fire(0.1, {
+        spread: 120,
+        startVelocity: 45,
+    });
+};
 //# sourceMappingURL=script.js.map
--- src/script.ts
+++ src/script.ts
@@ -9,63 +9,6 @@ type Task={
     createdAt:  Date,
 }
 
-const tasks:Task[]=[];
-button?.addEventListener('click', function(e){
-    e.preventDefault();
-    if(input?.value == "" || input?.value==null ){
-        return 
-    }
-    // input.value// after this point no longer null because we have an if statment that checks if its null so typescript knows afterr that point there is noprobablity of it being null
-
-    
-    const newtask:Task={
-        title: input.value,
-        completed: false,
-        createdAt: new Date()
-    }
-    tasks.push(newtask);
-    addTask( );
-    input.value="";
-    console.log(tasks);
-    saveTask();
-})
-
-const addTask = function():void{
-
-    if(list?.innerHTML){
-        list.innerHTML = "";
-    } 
-
-    tasks.forEach((task)=>{
-
-        const taskList = document.createElement("li");
-        const div =document.createElement("div")
-        div.setAttribute("class", "list-container");
-        const checkBox = document.createElement("input");
-        checkBox.setAttribute("type", "checkbox");
-        const label = document.createElement("label")
-        label.append(checkBox, task.title); 
-        // div.innerHTML= `<div class="div2">${label.innerHTML}</div><div class="icons"> <img src="../edit.png" onclick="remove(${index})"><img src="../delete.png" onclick="remove(${index})"></div>`
-        taskList.append(label);
-        list?.append(taskList);
-
-        checkBox.checked=task.completed;
-        checkBox.addEventListener("change", ()=>{       
-             task.completed=checkBox.checked;
-             console.log(tasks)
-             saveTask();
-             
-        })
-        // if(checkBox.checked){
-        //     label.setAttribute("class", ".completed")
-        //  }
-
-    })
-    
-    
-
-    
-}
 
 const saveTask= function():void{
     localStorage.setItem("tasks", JSON.stringify(tasks));
@@ -81,11 +24,187 @@ const getTask=function(){
      saved.forEach((task)=>{
         tasks.push(task)
      })
-
-     
-     
 }
+
 document.addEventListener("DOMContentLoaded",()=>{
     getTask();
     addTask();
+    updatedCount();
 })
+
+const tasks:Task[]=[];
+
+button?.addEventListener('click', function(e){
+    e.preventDefault();
+    if(input?.value == "" || input?.value==null ){
+        return 
+    }
+    // input.value// after this point no longer null because we have an if statment that checks if its null so typescript knows afterr that point there is noprobablity of it being null
+
+    const newtask:Task={
+        title: input.value,
+        completed: false,
+        createdAt: new Date()
+    }
+    
+    tasks.push(newtask);
+    addTask( );
+    input.value="";
+    console.log(tasks);
+    saveTask();
+    updatedCount();
+})
+
+
+const addTask = function():void{
+
+    if(list?.innerHTML){
+        list.innerHTML = "";
+    } 
+
+    tasks.forEach((task, index)=>{
+
+        const taskList = document.createElement("li");
+        const div =document.createElement("div")
+        div.setAttribute("class", "list-container");
+
+        const label = document.createElement("label")
+        const checkBox = document.createElement("input");
+        checkBox.setAttribute("type", "checkbox");
+
+        if(checkBox.checked){
+            label.setAttribute("class", "completed");
+            saveTask();
+           }
+         else
+         label.setAttribute("class", "");
+         label.append(checkBox, task.title);
+
+        const div2 = document.createElement("div");
+        div2.setAttribute("class", "div2");
+        div2.append(label);
+
+        const iconDiv = document.createElement("div");
+        iconDiv.setAttribute("class", "icons");
+
+        const deleteImg = document.createElement("img");
+        deleteImg.src = "../delete.png";
+        deleteImg.addEventListener("click", () => remove(index)); 
+
+        const editImg = document.createElement("img");
+        editImg.src = "../edit.png"; 
+        editImg.addEventListener("click", () => edit(index));
+
+        iconDiv.append(editImg, deleteImg);
+        div.append(div2, iconDiv);
+        taskList.append(div);
+        list?.append(taskList);
+
+        checkBox.checked=task.completed;
+        checkBox.addEventListener("change", ()=>{       
+             task.completed=checkBox.checked;
+             console.log(tasks);
+             if(checkBox.checked){
+                label.setAttribute("class", "completed");
+               }
+             else{
+             label.setAttribute("class", "");}
+             saveTask();
+             updatedCount();
+     
+        });
+
+        if(checkBox.checked){
+            label.setAttribute("class", "completed");
+           }
+         else
+         label.setAttribute("class", "");
+
+    })
+   
+}
+
+const remove =function(index:number){
+    tasks.splice(index, 1);
+    addTask();
+    saveTask();
+    updatedCount();
+}
+
+const edit = function(index:number){
+    if (input?.value ==null) {   
+    } else { 
+        input.value=tasks[index].title;
+        tasks.splice(index, 1);
+        addTask();  
+        saveTask(); 
+        updatedCount();
+    }
+}
+const updatedCount = ()=>{
+
+    let count = <HTMLParagraphElement>document.querySelector(".task-count p");
+    let progress = <HTMLDivElement>document.querySelector(".progress");
+    let message = <HTMLParagraphElement>document.querySelector(".title p");
+
+    let totalTask = tasks.length;
+    let completedTask = tasks.filter(task => task.completed).length;
+    count.innerHTML= completedTask +" / "+totalTask;
+    if(totalTask==0&&completedTask==0){
+        progress.style.width="0%";
+    }
+    else{
+        progress.style.width=completedTask/totalTask*100+"%";
+    }
+    
+
+    if(totalTask==completedTask&&totalTask!=0&&completedTask!=0){
+        message.innerHTML="You Completed Your Tasks!!"
+        confettiEffect();
+    }
+    else{
+        message.innerHTML="Keep it up"
+    }
+
+}
+const confettiEffect = (): void => {
+    const count = 200;
+    const defaults = {
+        origin: { y: 0.7 },
+    };
+
+    function fire(particleRatio: number, opts: object): void {
+        (window as any).confetti(
+            Object.assign({}, defaults, opts, {
+                particleCount: Math.floor(count * particleRatio),
+            })
+        );
+    }
+
+    fire(0.25, {
+        spread: 26,
+        startVelocity: 55,
+    });
+
+    fire(0.2, {
+        spread: 60,
+    });
+
+    fire(0.35, {
+        spread: 100,
+        decay: 0.91,
+        scalar: 0.8,
+    });
+
+    fire(0.1, {
+        spread: 120,
+        startVelocity: 25,
+        decay: 0.92,
+        scalar: 1.2,
+    });
+
+    fire(0.1, {
+        spread: 120,
+        startVelocity: 45,
+    });
+};
